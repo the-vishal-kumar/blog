b00962185451354eee0a9db471df0ef50dc6717a
Vishal Kumar <vishal194kumar@gmail.com>
2021-05-10 00:36:06 +0530

    build backend and frontend together fixed

---
+++ backend/index.ts
@@ -0,0 +1,24 @@
+import path from 'path'
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+console.log(`__dirname==>`, __dirname, `---__dirname`);
+app.use(express.static(path.join(__dirname, './frontend')))
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
